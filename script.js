897bb85c929e2daa741630f15afe78279ff96ded
Dhuzio <dany97@o2.pl>
2024-12-11 18:31:13 +0100

    added contact section

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-	const hamburger = document.querySelector('.nav__hamburger');
-	const menu = document.querySelector('.nav__menu');
-
-	hamburger.addEventListener('click', () => {
-		menu.classList.toggle('nav__menu--hidden');
-	});
-});
